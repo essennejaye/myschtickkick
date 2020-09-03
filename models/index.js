a1a94f6a54f395e456aba216c68027d2c2358233
heatherzen <heather.zen@gmail.com>
2020-09-03 14:12:54 -0700

    initial model set up

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require('./User');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+Post.belongTo(User, {
+    foreignKey: 'user_id'
+})
+
+module.exports = { User, Post };
