e52321056a979147da876d55f8492500d8141c63
essennejaye <sjeffers02@gmail.com>
2020-09-10 19:51:33 -0700

    files merged

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,9 +3,9 @@ const { Post, User } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth')
 
-router.use(fileUpload({
-    limits: { fileSize: 250 * 250 * 250 },
-  }));
+// router.use(fileUpload({
+//     limits: { fileSize: 250 * 250 * 250 },
+//   }));
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -108,22 +108,22 @@ router.post('/', (req, res) => {
         });
 });
 
-router.post('/upload', function(req, res) {
-    if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).send('No files were uploaded.');
-    }
+// router.post('/upload', function(req, res) {
+//     if (!req.files || Object.keys(req.files).length === 0) {
+//       return res.status(400).send('No files were uploaded.');
+//     }
   
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    let sampleFile = req.files.sampleFile;
+//     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+//     let sampleFile = req.files.sampleFile;
   
-    // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv('../public/uploads/filename.jpg', function(err) {
-      if (err)
-        return res.status(500).send(err);
+//     // Use the mv() method to place the file somewhere on your server
+//     sampleFile.mv('../public/uploads/filename.jpg', function(err) {
+//       if (err)
+//         return res.status(500).send(err);
   
-      res.send('File uploaded!');
-    });
-  });
+//       res.send('File uploaded!');
+//     });
+//   });
 
 router.put('/:id', withAuth, (req, res) => {
     Post.update(
--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const Post = require('./Post');
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
-    onDelete: CASCADE
+    onDelete: 'CASCADE'
 })
 
 Post.belongsTo(User, {
